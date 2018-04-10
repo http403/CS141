73a2678e24937b5b3ec95f904731bc1c03e670ae
Hartman Tam <hartmantam.enc@gmail.com>
2018-04-10 14:07:07 -0700

    make the nested loop look nicer

--- src/Day02DataAndLoops.java
+++ src/Day02DataAndLoops.java
@@ -42,8 +42,9 @@ public class Day02DataAndLoops {
     public static void nestedLoop() {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.println(i);
+                System.out.print(i);
             }
+            System.out.println();
         }
     }
 
