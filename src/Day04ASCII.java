253d8aa2ee97f88065c9dcca6cea724c465efd3b
Hartman Tam <hartmantam.enc@gmail.com>
2018-04-19 07:35:20 -0700

    initial commit

---
+++ src/Day04ASCII.java
@@ -0,0 +1,34 @@
+public class Day04ASCII {
+
+    public static void main(String[] args) {
+        numberTri();
+        hourglass();
+    }
+
+
+    /* This method should produce the following
+       -----1-----
+       ----333----
+       ---55555---
+       --7777777--
+       -999999999-
+    */
+    public static void numberTri() {
+    }
+
+    /* This method should produce the following
+       |""""""""""|
+        \::::::::/
+         \::::::/
+          \::::/
+           \::/
+            ||
+           /::\
+          /::::\
+         /::::::\
+        /::::::::\
+       |""""""""""|
+    */
+    public static void hourglass() {
+    }
+}
