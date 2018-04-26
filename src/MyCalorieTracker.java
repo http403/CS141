355368708982a7c33c8fc61c2d140b724c1de51e
Hartman Tam <hartmantam.enc@gmail.com>
2018-04-26 16:14:16 -0700

    rename some methods; add comments

--- src/MyCalorieTracker.java
+++ src/MyCalorieTracker.java
@@ -10,32 +10,44 @@ public class MyCalorieTracker {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        // get basic information
         welcome();
         int goal = setGoal(console);
+
+        // day 1
         int d1_intake = dailyIntake(console, 1);
         int d1_burned = dailyBurnedOut(console);
 
+        // day 2
         int d2_intake = dailyIntake(console, 2);
         int d2_burned = dailyBurnedOut(console);
 
+        // print summary
         int d1_net = summary(1, d1_intake, d1_burned);
         int d2_net = summary(2, d2_intake, d2_burned);
 
-        twoDayComparsion(goal, d1_net, d2_net);
+        // print misc.
+        printTwoDayComparision(goal, d1_net, d2_net);
         printTwoDaysAverage(d1_net, d2_net);
         printDisclaimer();
     }
+
+    // Print out welcome text
     public static void welcome() {
         System.out.println("This program compares your daily caloric intake\n" +
                 "for two days and determines which day was better\n" +
                 "in regards to reaching your fitness goals.\n");
     }
+
+    // Set daily calories goal
     public static int setGoal(Scanner console) {
         System.out.print("What is your calories goal for the day? > ");
         int goal = console.nextInt();
         System.out.println();
         return goal;
     }
+
+    // Get user's daily calories intake
     public static int dailyIntake(Scanner console, int day) {
         System.out.printf("Information for Day #%d:\n" +
                 "\t\thow many meals did you eat? > ", day);
@@ -48,6 +60,8 @@ public class MyCalorieTracker {
         System.out.printf("\t\t-- Total caloric intake = %d --\n", calories);
         return calories;
     }
+
+    // Get user's daily burned calories
     public static int dailyBurnedOut(Scanner console) {
         System.out.print("\t\twhat was your activity level? 1) not active 2) lightly active\n" +
                 "\t\t\t\t\t\t\t\t\t  3) active or 4) very active? ");
@@ -64,12 +78,16 @@ public class MyCalorieTracker {
         System.out.printf("\t\t-- Estimated active caloric burn = %d --\n\n", burned);
         return burned;
     }
+
+    // print summary; return net calories
     public static int summary(int day, int intake, int burned) {
-        int net = netCalores(intake, burned);
+        int net = netCalories(intake, burned);
         System.out.printf("Day %d Net Calories = %d\n", day, net);
         return net;
     }
-    public static void twoDayComparsion(int goal, int day1_net, int day2_net) {
+
+    // print comparision
+    public static void printTwoDayComparision(int goal, int day1_net, int day2_net) {
         int delta_d1 = Math.abs(goal - day1_net);
         int delta_d2 = Math.abs(goal - day2_net);
         if (delta_d1 == delta_d2) {
@@ -82,15 +100,23 @@ public class MyCalorieTracker {
             }
         }
     }
+
+    // print two days average
     public static void printTwoDaysAverage(int day1_net, int day2_net) {
         System.out.printf("Average Net Calories = %.1f\n\n", averageOfTwoNumbers(day1_net, day2_net));
     }
+
+    // average number helper function
     public static double averageOfTwoNumbers(double a, double b) {
         return (a + b) / 2;
     }
-    public static int netCalores(int intake, int burned) {
+
+    // net number helper function
+    public static int netCalories(int intake, int burned) {
         return intake - burned;
     }
+
+    // print disclaimer
     public static void printDisclaimer() {
         System.out.println("** DISCLAIMER: Summary results are probably wrong. I\n" +
                 "wouldn't trust them if I were you.");
@@ -134,5 +160,5 @@ public class MyCalorieTracker {
 
     ** DISCLAIMER: Summary results are probably wrong. I
     wouldn't trust them if I were you.
-    
+
 */
