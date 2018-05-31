f9676d59bed0841e8d65247617d47be15177a46e
Hartman Tam <hartmantam.enc@gmail.com>
2018-05-31 12:47:45 -0700

    add package header; indent 4 spaces

--- src/Day16/Day16MoreOOP.java
+++ src/Day16/Day16MoreOOP.java
@@ -6,44 +6,45 @@
   https://catalog.data.gov/dataset/seattle-pet-licenses
 */
 
+package Day16;
+
 import java.util.*;
 import java.io.*;
 
 public class Day16MoreOOP {
 
-   public static void main(String[] args) throws FileNotFoundException {
-      Scanner input = new Scanner(new File("Seattle_Pet_Licenses.csv"));
-      PetLicense[] petDB = loadInput(input);
-      System.out.println(Arrays.toString(petDB));
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(new File("Seattle_Pet_Licenses.csv"));
+        PetLicense[] petDB = loadInput(input);
+        System.out.println(Arrays.toString(petDB));
 
 //       String lookupName = "Love";
 //       int count = findByName(petDB, lookupName);
 //       System.out.printf("%s was found %d times in the database.",lookupName,count);
-   }
+    }
 
-   public static int findByName(PetLicense[] db, String name) {
-      return 0;
-   }
+    public static int findByName(PetLicense[] db, String name) {
+        return 0;
+    }
 
-   public static PetLicense[] loadInput(Scanner in) {
-      String header = in.nextLine(); //dump header
-      PetLicense[] data = new PetLicense[66043];
-      int i = 0;
+    public static PetLicense[] loadInput(Scanner in) {
+        String header = in.nextLine(); //dump header
+        PetLicense[] data = new PetLicense[66043];
+        int i = 0;
 
-      while(in.hasNextLine()) {
-         String row = in.nextLine();
-         String[] rowData = row.split(",");
-         if(rowData.length == 7 && rowData[6].length() == 5
-            && !rowData[6].replace(" ","").equals("") && !Character.isLetter(rowData[1].charAt(0))) {
-            //System.out.println(Arrays.toString(rowData));
-            int license = Integer.parseInt(rowData[1]);
-            int zip = Integer.parseInt(rowData[6]);
-            data[i++] = new PetLicense(rowData[0],license,rowData[2],rowData[3],rowData[4],rowData[5],zip);
-         }
-         else {
-            //System.out.println("bad data field(s): " + Arrays.toString(rowData));
-         }
-      }
-      return data;
-   }
+        while (in.hasNextLine()) {
+            String row = in.nextLine();
+            String[] rowData = row.split(",");
+            if (rowData.length == 7 && rowData[6].length() == 5
+                    && !rowData[6].replace(" ", "").equals("") && !Character.isLetter(rowData[1].charAt(0))) {
+                //System.out.println(Arrays.toString(rowData));
+                int license = Integer.parseInt(rowData[1]);
+                int zip = Integer.parseInt(rowData[6]);
+                data[i++] = new PetLicense(rowData[0], license, rowData[2], rowData[3], rowData[4], rowData[5], zip);
+            } else {
+                //System.out.println("bad data field(s): " + Arrays.toString(rowData));
+            }
+        }
+        return data;
+    }
 }
--- src/Day16/Pet.java
+++ src/Day16/Pet.java
@@ -1,18 +1,20 @@
+package Day16;
+
 public class Pet {
 
-   private String name;
-   private String species;
-   private String primaryBreed;
-   private String secondaryBreed;
+    private String name;
+    private String species;
+    private String primaryBreed;
+    private String secondaryBreed;
 
-   public Pet() {
-   }   
+    public Pet() {
+    }
 
-   public Pet(String name, String species, String primaryBreed, String secondaryBreed) {
-      this.name = name;
-      this.species = species;
-      this.primaryBreed = primaryBreed;
-      this.secondaryBreed = secondaryBreed;
-   }
+    public Pet(String name, String species, String primaryBreed, String secondaryBreed) {
+        this.name = name;
+        this.species = species;
+        this.primaryBreed = primaryBreed;
+        this.secondaryBreed = secondaryBreed;
+    }
 
 }
