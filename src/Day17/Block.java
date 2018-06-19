37b963420cf35ba2686514b60a044a5e1ca368d8
Hartman Tam <hartmantam.enc@gmail.com>
2018-06-19 07:50:09 -0700

    acm graphic tutorial

---
+++ src/Day17/Block.java
@@ -0,0 +1,27 @@
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+
+public class Block extends GObject {
+   
+   private int width;
+
+   public Block() {
+      this(250,250);
+   }
+   
+   public Block(int x, int y) {
+      setLocation(x,y);
+      setColor(Color.BLACK);
+      this.width = 20;
+   }
+
+   public GRectangle getBounds() {
+      return new GRectangle(getX(), getY(), this.width, this.width);
+   }
+   
+   public void paint(Graphics g) {
+      g.fillRect((int)getX(), (int)getY(), (int)getWidth(), (int)getHeight());
+   }
+
+}
