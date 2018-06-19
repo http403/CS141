136705b6c95bf50a8bceb574edb7b35c7dda2a1e
Hartman Tam <hartmantam.enc@gmail.com>
2018-06-19 07:49:01 -0700

    create boss from employee

--- src/Day17/Boss.java
+++ src/Day17/Boss.java
@@ -14,3 +14,30 @@
 */
 
 package Day17;
+
+public class Boss extends Employee {
+
+    public Boss() {
+        super("Boss");
+    }
+
+    public Boss(String name) {
+        super(name);
+    }
+
+    public String toString() {
+        return String.format("<Boss id=%d name=$s>", this.getId(), this.getName());
+    }
+
+    public double getSalary() { return super.getSalary() * 4; }
+    public int getHours() { return super.getHours() * 2; }
+    public int getVacationDays() { return 0; }
+    public String getVacationForm() { return ""; }
+    public void applyForVacation() { System.out.println("The big boss get no vacay!"); }
+
+    //add behavior
+    public void promote(Employee e) {
+        e.setSalary(e.getSalary() * 1.1);
+        e.setVacationDays(e.getVacationDays() + 2);
+    }
+}
