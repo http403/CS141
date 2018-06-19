b0d10f9c0545cf942ad070be02a195eb3037942a
Hartman Tam <hartmantam.enc@gmail.com>
2018-06-19 07:53:33 -0700

    add package header; reformat code

--- src/Day17/BlockProgram.java
+++ src/Day17/BlockProgram.java
@@ -1,24 +1,27 @@
+package Day17;
+
 import acm.program.*;
 import acm.graphics.*;
-import java.awt.*;
- 
-public class BlockProgram extends GraphicsProgram {
- 
-   private Block b;
- 
-   public void init() {
-      b = new Block(100,100);
-      add(b);
-   }
 
-   public void run() {
-      while(true) {
-         b.move(2,2);
-         pause(500);
-      }
-   }
-   
-   public static void main(String[] args) {
-		new BlockProgram().start(args);
-	}
+import java.awt.*;
+
+public class BlockProgram extends GraphicsProgram {
+
+    private Block b;
+
+    public void init() {
+        b = new Block(100, 100);
+        add(b);
+    }
+
+    public void run() {
+        while (true) {
+            b.move(2, 2);
+            pause(500);
+        }
+    }
+
+    public static void main(String[] args) {
+        new BlockProgram().start(args);
+    }
 } 
