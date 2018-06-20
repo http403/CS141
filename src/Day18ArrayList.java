a5b1ccb274667ee545c4aa87f0a98f1517ea0ca9
Hartman Tam <hartmantam.enc@gmail.com>
2018-06-20 11:02:26 -0700

    done

--- src/Day18ArrayList.java
+++ src/Day18ArrayList.java
@@ -1,26 +1,27 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Day18ArrayList {
     public static void main(String[] args) {
-        learnArrayList();
+//        learnArrayList();
 
-//       ArrayList<Integer> values = createList(5,200);
-//       System.out.println("initial values = " + values);
-//       
-//       duplicateList(values);
-//       //maxToEnd(values);
-//       
-//       System.out.println("values = " + values);
+//        ArrayList<Integer> values = createList(5, 200);
+//        System.out.println("initial values = " + values);
+//
+////        duplicateList(values);
+//        maxToEnd(values);
+//
+//        System.out.println("values = " + values);
 
-        // ArrayList<String> words = createWordList();
-        // System.out.println("words contains " + words);
-        //
-        // int len = maxLength(words);
-        // System.out.println("highest # of chars = " + len);
+        ArrayList<String> words = createWordList();
+        System.out.println("words contains " + words);
 
-        // removeStartWith(words,'c');
-        // System.out.println("words contains " + words);
+//        int len = maxLength(words);
+//        System.out.println("highest # of chars = " + len);
+
+        removeStartWith(words, 'c');
+        System.out.println("words contains " + words);
     }
 
     public static void learnArrayList() {
@@ -38,33 +39,61 @@ public class Day18ArrayList {
     // where each int is a random number between 1 and 'high'
     // e.g. createList(3,15) might return [13, 1, 9]
     public static ArrayList<Integer> createList(int numElements, int high) {
-        return null;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < numElements; i++) {
+            list.add(new Random().nextInt(high) + 1);
+        }
+        return list;
     }
 
     // duplicates a list of Integers by appending each element in the list
     // to the end of the list
     // e.g. [1,2,3] ==> [1,2,3,1,2,3]
     public static void duplicateList(ArrayList<Integer> list) {
+//        for (int i = 0; i < list.size(); i++) {
+//            list.add(list.get(i));
+//        }
+        list.addAll(list);
     }
 
     // this method moves the max value in a list of Integers to the end
     // e.g. [1,29,54,2] ==> [1,29,2,54]
     public static void maxToEnd(ArrayList<Integer> list) {
+        int max = list.get(0);
+        for (int x : list) {
+            max = Math.max(max, x);
+        }
+        list.remove(list.indexOf(max));
+        list.add(max);
     }
 
     // this method returns a list of Strings
     public static ArrayList<String> createWordList() {
-        return null;
+        ArrayList<String> list = new ArrayList<String>();
+        Scanner words = new Scanner("carrots,cabbage,red peppers,celery");
+        words.useDelimiter(",");
+        while (words.hasNext()) {
+            list.add(words.next());
+        }
+        return list;
     }
 
     // this method returns the length of the longest String
     // e.g. ["hi", "hello", "hola", "yo"] ==> 5
     public static int maxLength(ArrayList<String> list) {
-        return 0;
+        int max = Integer.MIN_VALUE;
+        for (String s : list)
+            max = Math.max(max, s.length());
+        return max;
     }
 
     // remove all elements in list that begin with specified char
     public static void removeStartWith(ArrayList<String> list, char c) {
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i).charAt(0) == c) {
+                list.remove(i);
+            }
+        }
     }
 }
 
